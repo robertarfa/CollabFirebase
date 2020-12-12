105af288ee54ba1444a2d1390f077443f8c20b8e
Roberta Aguilar <roberta_rfa@hotmail.com>
2020-12-12 18:20:22 -0300

    changes

--- src/services/store/configureStore.js
+++ src/services/store/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 
